84dfceb43f35957345950efcddde8e0ccbacb309
Nat379 <nat.sh379@gmail.com>
2023-11-17 12:34:56 +0200

    feat: complete 48 tests

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -107,8 +107,14 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magnitude1 = Math.sqrt(x1 * x1 + y1 * y1);
+  const magnitude2 = Math.sqrt(x2 * x2 + y2 * y2);
+  const cosAngle = dotProduct / (magnitude1 * magnitude2);
+  const angleInRadians = Math.acos(cosAngle);
+
+  return angleInRadians;
 }
 
 /**
@@ -124,8 +130,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -177,8 +183,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const divisor = 10 ** pow;
+  return Math.round(num / divisor) * divisor;
 }
 
 /**
@@ -198,8 +205,28 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+
+  if (n <= 3) {
+    return true;
+  }
+
+  if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+
+  let i = 5;
+  while (i * i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) {
+      return false;
+    }
+    i += 6;
+  }
+
+  return true;
 }
 
 /**
